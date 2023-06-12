4f79550d18b67ba5e4378a164110c0c39b178c20
KaceyXam <kaceykniffen@gmail.com>
2023-06-12 18:45:09 -0500

    Got LSP servers working

--- init.lua
+++ init.lua
@@ -44,22 +44,23 @@ require("lazy").setup({
         "nvim-lualine/lualine.nvim",
     },
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-                'williamboman/mason.nvim',
-                build = ":MasonUpdate",
-            },
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+        ensure_installed = {
+            "rust-analyzer",
+            "ols",
         }
     },
+    { "williamboman/mason-lspconfig.nvim" },
+    { 
+        "neovim/nvim-lspconfig",
+        config = function()
+        end
+    },
+    { "hrsh7th/nvim-cmp" },
+    { "L3MON4D3/LuaSnip" },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { "hrsh7th/cmp-nvim-lsp" },
     {
         "nvim-lua/plenary.nvim",
     },
@@ -69,21 +70,96 @@ require("lazy").setup({
     }
 })
 
+-- Treesitter Setup
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "lua", "rust", "odin", "go" },
+
+  sync_install = false,
+
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    additional_vim_regex_highlighting = false,
+  },
+}
+
 -- Lualine Setup
 require("lualine").setup {
     options = { theme = "gruvbox" }
 }
 
--- Lsp-Zero setup
-local lsp = require("lsp-zero").preset({})
+-- Lsp setup
+require("mason").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "ols", "rust_analyzer" }
+})
 
-lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({buffer = bufnr})
-end)
+local on_attach = function(_, _)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}) 
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {}) 
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {}) 
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 
-lsp.setup()
+end
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig").lua_ls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
+
+require("lspconfig").ols.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+
+require("lspconfig").rust_analyzer.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        ['rust-analyzer'] = {
+            cargo = {
+                allFeatures = true,
+            },
+        }
+    }
+})
+
+-- Nvim CMP setup
+local cmp = require("cmp")
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+    }, {
+        { name = "buffer" },
+    }),
+})
 
 -- Telescope Setup
 local builtin = require('telescope.builtin')
